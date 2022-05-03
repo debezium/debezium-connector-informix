a0586c66f85e145720de8f6a597fd9b222386f54
Xiaolin Zhang <leoncamel@gmail.com>
2022-05-03 16:08:24 +0800

    DBZ-4999 Rewrite Lsn/TxLogPosition in Java

--- src/test/java/laoflch/debezium/connector/informix/LsnTest.java
+++ src/test/java/laoflch/debezium/connector/informix/LsnTest.java
@@ -1,5 +1,7 @@
 package laoflch.debezium.connector.informix;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import io.debezium.relational.history.HistoryRecordComparator;
 import org.testng.annotations.Test;
 
@@ -15,22 +17,22 @@ public class LsnTest {
 
     @Test
     void testLsnNULL() {
-        Lsn recorded = Lsn.NULL();
+        Lsn recorded = Lsn.NULL;
         Lsn desired = Lsn.valueOf("146031505564");
         int ret = recorded.compareTo(desired);
-        System.out.println(ret);
+        assertThat(recorded.compareTo(desired)).isEqualTo(-1);
 
-        Lsn recorded1 = Lsn.valueOf("null");
+        Lsn recorded1 = Lsn.valueOf("NULL");
         Lsn desired1 = Lsn.valueOf("146031505564");
-        int ret1 = recorded.compareTo(desired);
-        System.out.println(ret1);
+        assertThat(recorded1.compareTo(desired1)).isEqualTo(-1);
     }
 
     @Test
-    void testLsnZero() {
-        Lsn recorded = Lsn.valueOf("1");
-        Lsn desired = Lsn.valueOf("146031505564");
-        int ret = recorded.compareTo(desired);
-        System.out.println(ret);
+    void testValueOf() {
+        String lsnStr1 = "146039087264";
+        String lsnStr2 = "14603908:7264";
+        Lsn lsn1 = Lsn.valueOf(lsnStr1);
+        Lsn lsn2 = Lsn.valueOf(lsnStr2);
+        assertThat(lsn1).isEqualTo(lsn2);
     }
 }
