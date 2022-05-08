2b59a832e63897e514e7a74354db80690476df92
Xiaolin Zhang <leoncamel@gmail.com>
2022-05-08 19:11:04 +0800

    DBZ-4999 Rewrite InformixConnector/InformixConnectorTask into Java

---
+++ src/main/java/laoflch/debezium/connector/informix/InformixConnector.java
@@ -0,0 +1,50 @@
+package laoflch.debezium.connector.informix;
+
+import io.debezium.annotation.ThreadSafe;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ThreadSafe
+public class InformixConnector extends SourceConnector {
+
+    private Map<String, String> properties;
+
+    @Override
+    public String version() {
+        return Module.version();
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        this.properties = Collections.unmodifiableMap(new HashMap<>(props));
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return InformixConnectorTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        if (maxTasks > 1) {
+            throw new IllegalArgumentException("Only a single connector task may be started");
+        }
+
+        return Collections.singletonList(properties);
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public ConfigDef config() {
+        return InformixConnectorConfig.configDef();
+    }
+}
