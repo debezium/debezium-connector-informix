9ead9b8dfc55e65381356cb37438826787c1adcb
Roman Kudryashov <rskudryashov@gmail.com>
2024-04-07 09:58:39 +0300

    DBZ-7755 Refactor exporting to CloudEvents: remove RecordParser

--- src/main/java/io/debezium/connector/informix/converters/InformixCloudEventsMaker.java
+++ src/main/java/io/debezium/connector/informix/converters/InformixCloudEventsMaker.java
@@ -5,10 +5,14 @@
  */
 package io.debezium.connector.informix.converters;
 
+import java.util.Set;
+
 import io.debezium.connector.AbstractSourceInfo;
+import io.debezium.converters.recordandmetadata.RecordAndMetadata;
 import io.debezium.converters.spi.CloudEventsMaker;
-import io.debezium.converters.spi.RecordParser;
 import io.debezium.converters.spi.SerializerType;
+import io.debezium.data.Envelope;
+import io.debezium.util.Collect;
 
 /**
  * CloudEvents maker for records produced by the Informix connector.
@@ -18,14 +22,24 @@ import io.debezium.converters.spi.SerializerType;
  */
 public class InformixCloudEventsMaker extends CloudEventsMaker {
 
-    public InformixCloudEventsMaker(RecordParser parser, SerializerType contentType, String dataSchemaUriBase, String cloudEventsSchemaName) {
-        super(parser, contentType, dataSchemaUriBase, cloudEventsSchemaName);
+    static final String CHANGE_LSN_KEY = "change_lsn";
+    static final String COMMIT_LSN_KEY = "commit_lsn";
+
+    static final Set<String> IFX_SOURCE_FIELDS = Collect.unmodifiableSet(CHANGE_LSN_KEY, COMMIT_LSN_KEY);
+
+    public InformixCloudEventsMaker(RecordAndMetadata recordAndMetadata, SerializerType contentType, String dataSchemaUriBase, String cloudEventsSchemaName) {
+        super(recordAndMetadata, contentType, dataSchemaUriBase, cloudEventsSchemaName, Envelope.FieldName.BEFORE, Envelope.FieldName.AFTER);
     }
 
     @Override
     public String ceId() {
-        return "name:" + recordParser.getMetadata(AbstractSourceInfo.SERVER_NAME_KEY)
-                + ";change_lsn:" + recordParser.getMetadata(InformixRecordParser.CHANGE_LSN_KEY)
-                + ";commit_lsn:" + recordParser.getMetadata(InformixRecordParser.COMMIT_LSN_KEY);
+        return "name:" + sourceField(AbstractSourceInfo.SERVER_NAME_KEY)
+                + ";change_lsn:" + sourceField(CHANGE_LSN_KEY)
+                + ";commit_lsn:" + sourceField(COMMIT_LSN_KEY);
+    }
+
+    @Override
+    public Set<String> connectorSpecificSourceFields() {
+        return IFX_SOURCE_FIELDS;
     }
 }
