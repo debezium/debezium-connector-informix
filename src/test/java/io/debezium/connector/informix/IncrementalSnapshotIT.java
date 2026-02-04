74c520c70f37b7745e9a7b472d6a8effb4d3c8c0
Lars M Johansson <lars.m.johansson@migrationsverket.se>
2026-02-04 16:13:10 +0100

    debezium/dbz#1129 * fix: stabilize IncrementalSnapshotIT
    Signed-off-by: Lars M Johansson <lars.m.johansson@migrationsverket.se>

--- src/test/java/io/debezium/connector/informix/IncrementalSnapshotIT.java
+++ src/test/java/io/debezium/connector/informix/IncrementalSnapshotIT.java
@@ -21,7 +21,6 @@ import io.debezium.connector.informix.InformixConnectorConfig.SnapshotMode;
 import io.debezium.connector.informix.util.TestHelper;
 import io.debezium.jdbc.JdbcConnection;
 import io.debezium.junit.ConditionalFailExtension;
-import io.debezium.junit.Flaky;
 import io.debezium.pipeline.source.snapshot.incremental.AbstractIncrementalSnapshotTest;
 
 @ExtendWith(ConditionalFailExtension.class)
@@ -174,14 +173,12 @@ public class IncrementalSnapshotIT extends AbstractIncrementalSnapshotTest<Infor
     }
 
     @Test
-    @Flaky("DBZ-9475")
     @Override
     public void removeNotYetCapturedCollectionFromInProgressIncrementalSnapshot() throws Exception {
         super.removeNotYetCapturedCollectionFromInProgressIncrementalSnapshot();
     }
 
     @Test
-    @Flaky("DBZ-9475")
     @Override
     public void removeStartedCapturedCollectionFromInProgressIncrementalSnapshot() throws Exception {
         super.removeStartedCapturedCollectionFromInProgressIncrementalSnapshot();
